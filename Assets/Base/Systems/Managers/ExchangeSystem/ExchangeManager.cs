269b2d1a62bc9bd1ea1a0adb819d8a4fb89dcbd8
batuhanmn <37012160+batuhanmn@users.noreply.github.com>
2021-05-23 00:01:18 +0300

    Exchange Manager

---
+++ Assets/Base/Systems/Managers/ExchangeSystem/ExchangeManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ExchangeType{
+    Coin
+}
+
+public class ExchangeManager : Singleton<ExchangeManager>
+{
+    private bool isLoaded;
+    private Dictionary<ExchangeType, float> exchangeDictionary = new Dictionary<ExchangeType, float>();
+
+    public void DoExchange(ExchangeType type, float amount)
+    {
+        if (!isLoaded) Load();
+        if (!exchangeDictionary.ContainsKey(type))
+        {
+            exchangeDictionary[type] = 0;
+        }
+        exchangeDictionary[type]+= amount;
+        DataManager.Instance.SaveData("ExchangeData", exchangeDictionary);
+    }
+
+    public void Load()
+    {
+        isLoaded = true;
+        exchangeDictionary = DataManager.Instance.GetData<Dictionary<ExchangeType, float>>("ExchangeData");
+    }
+
+    public float GetExchange(ExchangeType type)
+    {
+        if (!exchangeDictionary.ContainsKey(type)) return 0;
+        return exchangeDictionary[type];
+    }
+
+    public void StateExchange(ExchangeType type, float amount)
+    {
+        if (!isLoaded) Load();
+        exchangeDictionary[type] = amount;
+        DataManager.Instance.SaveData("ExchangeData", exchangeDictionary);
+    }
+
+}
